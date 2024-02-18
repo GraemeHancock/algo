ab9e24c64f8a2cabf7f0fef073750587ff2f0131
Graeme Hancock <77099937+graemeSaSoft@users.noreply.github.com>
2024-02-18 18:33:10 +0200

    Updated

--- Clang/search/linear_search.c
+++ Clang/search/linear_search.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 
+int linear_search(const int haystack[], const int needle ) {
+
+    const int size = sizeof(haystack) / sizeof(haystack[0]);
+    for(int i = 0; i < size; i++) {
+        if(haystack[i], needle)
+        {
+            return 1;
+        }
+
+    }
+
+    return 0;
+}
+
+ 
+
+
 int main() {
-    return 0
+
+    
+    const int 
+
+
+    return 0;
 }
